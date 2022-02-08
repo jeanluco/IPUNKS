9af138e954713b1a81d7872135ca58e4774767dc
jeanluco <30448882+jeanluco@users.noreply.github.com>
2022-02-07 20:35:27 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,12 +6,7 @@ import store from "./redux/store";
 import { Provider } from "react-redux";
 import "./styles/reset.css";
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
